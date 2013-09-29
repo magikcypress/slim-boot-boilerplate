961f93a45c245651b8f7b2adae23eddba4987fa4
magikcypress <cyp@rouquin.me>
2013-09-30 00:17:21 +0200

    fix config

--- cache/b8/5f/e7d0680541fa5ec30868592ac125.php
+++ cache/b8/5f/e7d0680541fa5ec30868592ac125.php
@@ -51,14 +51,34 @@ class __TwigTemplate_b85fe7d0680541fa5ec30868592ac125 extends Twig_Template
         $this->displayBlock('content', $context, $blocks);
         // line 27
         echo "\t\t</div>
-
+\t\t
+\t\t<div class=\"container\">
+\t\t\t\t<ul class=\"inline\">
+\t\t\t\t\t<li>&copy; 2013</li>
+\t\t\t\t\t<li>&minus;</li>
+\t\t\t\t\t<li><a href=\"\">";
+        // line 33
+        if (isset($context["SITE_NAME"])) { $_SITE_NAME_ = $context["SITE_NAME"]; } else { $_SITE_NAME_ = null; }
+        echo twig_escape_filter($this->env, $_SITE_NAME_, "html", null, true);
+        echo "</a></li>
+\t\t\t\t\t<li>&minus;</li>
+\t\t\t\t\t<li><a href=\"";
+        // line 35
+        if (isset($context["LICENCE_URL"])) { $_LICENCE_URL_ = $context["LICENCE_URL"]; } else { $_LICENCE_URL_ = null; }
+        echo twig_escape_filter($this->env, $_LICENCE_URL_, "html", null, true);
+        echo "\">";
+        if (isset($context["LICENCE"])) { $_LICENCE_ = $context["LICENCE"]; } else { $_LICENCE_ = null; }
+        echo twig_escape_filter($this->env, $_LICENCE_, "html", null, true);
+        echo "</a></li>
+\t\t\t\t</ul>
+\t\t</div>
 \t\t";
-        // line 29
+        // line 38
         if (array_key_exists("GOOGLE_UA_ID", $context)) {
-            // line 30
+            // line 39
             echo "\t\t<script>
 \t\t\tvar _gaq = [['_setAccount', '";
-            // line 31
+            // line 40
             if (isset($context["GOOGLE_UA_ID"])) { $_GOOGLE_UA_ID_ = $context["GOOGLE_UA_ID"]; } else { $_GOOGLE_UA_ID_ = null; }
             echo twig_escape_filter($this->env, $_GOOGLE_UA_ID_, "html", null, true);
             echo "'], ['_trackPageview'], ['_trackPageLoadTime']];
@@ -71,7 +91,7 @@ class __TwigTemplate_b85fe7d0680541fa5ec30868592ac125 extends Twig_Template
 \t\t</script>
 \t\t";
         }
-        // line 40
+        // line 49
         echo "
 \t</body>
 </html>";
@@ -101,6 +121,6 @@ class __TwigTemplate_b85fe7d0680541fa5ec30868592ac125 extends Twig_Template
 
     public function getDebugInfo()
     {
-        return array (  87 => 26,  81 => 4,  75 => 40,  62 => 31,  59 => 30,  57 => 29,  53 => 27,  51 => 26,  26 => 4,  21 => 1,);
+        return array (  107 => 26,  101 => 4,  95 => 49,  82 => 40,  79 => 39,  77 => 38,  67 => 35,  61 => 33,  53 => 27,  51 => 26,  26 => 4,  21 => 1,);
     }
 }
